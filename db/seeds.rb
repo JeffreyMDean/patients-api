fe877a3cab6646c6fd4b37d73a9337a370a25a8e
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-06-04 13:31:16 -0400

    Added model with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+patient1 = Patient.new(name: "Carl", age: 42, insurance: "Anthem")
+patient1.save
+
+patient2 = Patient.new(name: "Alice", age: 35, insurance: "Medical Mutual")
+patient2.save
+
+patient3 = Patient.new(name: "Anthony", age: 20, insurance: "Oscar")
+patient3.save
